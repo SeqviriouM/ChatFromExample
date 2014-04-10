12fdf67fb8d36dbd20f96ef16613f92bfa7ed2f9
Roman Fedorenkov <suffering123494@yandex.ru>
2014-04-10 21:44:56 +0400

    created functions working with database

---
+++ createDb.js
@@ -0,0 +1,45 @@
+var mongoose = require('./libs/mongoose');
+var async = require('async');
+
+
+async.series([
+	open,
+	dropDatabase,
+	requireModels,
+	createUsers
+], function(err, results) {
+	console.log(arguments);
+	mongoose.disconnect();
+	process.exit(err ? 255 : 0);
+})
+
+function open(callback) {
+	mongoose.connection.on('open', callback);
+}
+
+function dropDatabase(callback) {
+	var db = mongoose.connection.db;
+	db.dropDatabase(callback);
+}
+
+function requireModels(callback) {
+	require('./models/user');
+
+	async.each(Object.keys(mongoose.models), function(modelName, callback) {
+		mongoose.models[modelName].ensureIndexes(callback);
+	}, callback);
+}
+
+function createUsers(callback) {
+	// Создаем трех юзеров и сохраняем в базу данных
+	var users = [
+		{username: "Вася", password: "supervasya"},
+		{username: "Петя", password: "123"},
+		{username: "admin", password: "thetruehero"}
+	];
+
+	async.each(users, function(userData, callback) {
+		var user = new mongoose.models.User(userData);
+		user.save(callback);
+	}, callback);
+}
